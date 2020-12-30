efd3401b6a6aef66d818c1bbf09e6084d813ca38
Micaela <mika.villagra09@gmail.com>
2020-12-30 17:10:48 -0300

    cambios

---
+++ workspace/JavaScript/js/mostrar-ocultar.js
@@ -0,0 +1,26 @@
+// Ubicar elemento/s dentro del DOM
+
+  var caja = document.getElementsById(Element="caja");
+
+// Generar funciones
+
+  function ocultar()
+  {
+    //caja.stilyle.visibility = "hidden";
+    //caja.style.opacity=0;
+    caja.style.display= "none";
+
+  }
+  function mostrar()
+  {
+    //caja.stilyle.visibility = "visible";
+    //caja.style.opacity=1;
+    caja.style.display= "block";
+
+  }
+
+  
+  
+
+ 
+   
